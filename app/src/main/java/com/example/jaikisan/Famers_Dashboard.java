ad0556d77e0cb1b17148ac2c3b2378531724bf9d
Madhan M <madhanmandy3374@gmail.com>
2021-07-24 23:28:43 +0530

    Started farmer's dashboard

---
+++ app/src/main/java/com/example/jaikisan/Famers_Dashboard.java
@@ -0,0 +1,14 @@
+package com.example.jaikisan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Famers_Dashboard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_famers_dashboard);
+    }
+}
