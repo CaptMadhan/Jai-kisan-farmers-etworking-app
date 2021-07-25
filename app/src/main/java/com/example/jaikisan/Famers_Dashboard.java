d14de4c3f28be0eb1290346efc43537d659ee948
Madhan M <madhanmandy3374@gmail.com>
2021-07-26 00:37:43 +0530

    Farmer's Login completed

--- app/src/main/java/com/example/jaikisan/Famers_Dashboard.java
+++ app/src/main/java/com/example/jaikisan/Famers_Dashboard.java
@@ -1,14 +1,36 @@
 package com.example.jaikisan;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Famers_Dashboard extends AppCompatActivity {
-
+    Button add_itemButton,remove_itemButton;
+    RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_famers_dashboard);
+        add_itemButton = findViewById(R.id.add_item_button);
+        remove_itemButton=findViewById(R.id.remove_item_button);
+        recyclerView=findViewById(R.id.recyclerview);
+    }
+
+    public void add_Item_Function(View view) {
+    }
+
+    public void remove_item_function(View view) {
+    }
+// CardView Add Item
+    public void cancel_item(View view) {
+    }
+
+    public void clearItemCard(View view) {
+    }
+
+    public void addItem_to_db(View view) {
     }
 }
--- app/src/main/java/com/example/jaikisan/MainActivity.java
+++ app/src/main/java/com/example/jaikisan/MainActivity.java
@@ -1,31 +1,56 @@
 package com.example.jaikisan;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseException;
+import com.google.firebase.FirebaseTooManyRequestsException;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthInvalidCredentialsException;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthOptions;
+import com.google.firebase.auth.PhoneAuthProvider;
 
 import java.util.Objects;
+import java.util.concurrent.TimeUnit;
+
+import static android.content.ContentValues.TAG;
 
 public class MainActivity extends AppCompatActivity {
-    Intent intent;
     Button kisanButton,consumerButton;
     ImageView coverPhoto;
 
-    CardView kisanCardView,consumerCardView;
+    CardView kisanCardView,consumerCardView,KisanCreateAccountCardView;
 
-    EditText kisanPhoneNumber,kisanPassword;
+    EditText kisanPhoneNumber,kisanOTP;
     Button kisanLoginButton,kisanCreateButton;
     TextView kisanForgotPasswordText,consumerForgotPasswordText;
 
     EditText consumerPhoneNumber,consumerPassword;
     Button consumerLoginButton,consumerCreateButton;
+
+    EditText farmerFullNameAC,farmer_stateAC,farmer_cityAC,farmer_AddressAC,farmer_PhoneNumberAC,farmer_verify_otp_AC;
+    Button farmer_get_otp_AC,finalCreateAC;
+
+    FirebaseAuth mAuth;
+    String codeSent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,9 +66,8 @@ public class MainActivity extends AppCompatActivity {
 
         kisanLoginButton=findViewById(R.id.KisanLoginButton);
         kisanPhoneNumber=findViewById(R.id.KisanPhoneNumber);
-        kisanPassword=findViewById(R.id.KisanPassword);
+        kisanOTP=findViewById(R.id.KisanOTPEdit);
         kisanCreateButton=findViewById(R.id.KisanCreateAccount);
-        kisanForgotPasswordText=findViewById(R.id.KisanResetPassword);
 
         consumerLoginButton=findViewById(R.id.ConsumerLoginButton);
         consumerPhoneNumber=findViewById(R.id.ConsumerPhoneNumber);
@@ -51,28 +75,139 @@ public class MainActivity extends AppCompatActivity {
         consumerCreateButton=findViewById(R.id.ConsumerCreateAccount);
         consumerForgotPasswordText=findViewById(R.id.ConsumerResetPassword);
 
+        KisanCreateAccountCardView =findViewById(R.id.KisanCreateAccountCardView);
+
+        farmerFullNameAC =findViewById(R.id.farmerFullNameAC);
+        farmer_stateAC=findViewById(R.id.farmer_stateAC);
+        farmer_cityAC=findViewById(R.id.farmer_cityAC);
+        farmer_AddressAC=findViewById(R.id.farmer_AddressAC);
+        farmer_PhoneNumberAC=findViewById(R.id.farmer_PhoneNumberAC);
+        farmer_verify_otp_AC=findViewById(R.id.farmer_verify_otp_AC);
+
+        farmer_get_otp_AC=findViewById(R.id.farmer_get_otp_AC);
+        finalCreateAC=findViewById(R.id.finalCreateAC);
+
+        mAuth = FirebaseAuth.getInstance();
+
     }
 
     public void FarmerLogin(View view) {
         consumerCardView.setVisibility(View.GONE);
         kisanCardView.setVisibility(View.VISIBLE);
+        KisanCreateAccountCardView.setVisibility(View.GONE);
     }
 
     public void ConsumerLogin(View view) {
         consumerCardView.setVisibility(View.VISIBLE);
         kisanCardView.setVisibility(View.GONE);
+        KisanCreateAccountCardView.setVisibility(View.GONE);
     }
+    public void KisanGenerateOTPButton(View view) {
+        String tempPhoneNumber = kisanPhoneNumber.getText().toString();
+        String phoneNumber = "+91"+tempPhoneNumber;
+        if(phoneNumber.length() != 13){
+            kisanPhoneNumber.setText("");
+            kisanPhoneNumber.requestFocus();
+            Toast.makeText(getApplicationContext(),"Enter Valid Phone Number",Toast.LENGTH_LONG).show();
+            return;
+        }
 
+        PhoneAuthProvider.OnVerificationStateChangedCallbacks mCallbacks;
+        mCallbacks = new PhoneAuthProvider.OnVerificationStateChangedCallbacks() {
+
+            @Override
+            public void onVerificationCompleted(PhoneAuthCredential credential) {
+            }
+
+            @Override
+            public void onVerificationFailed(FirebaseException e) {
+                // This callback is invoked in an invalid request for verification is made,
+                // for instance if the the phone number format is not valid.
+                Log.w(TAG, "onVerificationFailed", e);
+
+                if (e instanceof FirebaseAuthInvalidCredentialsException) {
+                    // Invalid request
+                } else if (e instanceof FirebaseTooManyRequestsException) {
+                    Toast.makeText(getApplicationContext(),"SMS quota for the project has been exceeded",Toast.LENGTH_LONG).show();
+                }
+                // Show a message and update the UI
+            }
+
+            @Override
+            public void onCodeSent(@NonNull String verificationId,
+                                   @NonNull PhoneAuthProvider.ForceResendingToken token) {
+                // The SMS verification code has been sent to the provided phone number, we
+                // now need to ask the user to enter the code and then construct a credential
+                // by combining the code with a verification ID.
+                Log.d(TAG, "onCodeSent:" + verificationId);
+
+                // Save verification ID and resending token so we can use them later
+                codeSent = verificationId;
+                //mResendToken = token;
+            }
+        };
+        PhoneAuthOptions options =
+                PhoneAuthOptions.newBuilder(mAuth)
+                        .setPhoneNumber(phoneNumber)       // Phone number to verify
+                        .setTimeout(60L, TimeUnit.SECONDS) // Timeout and unit
+                        .setActivity(this)                 // Activity (for callback binding)
+                        .setCallbacks(mCallbacks)          // OnVerificationStateChangedCallbacks
+                        .build();
+        PhoneAuthProvider.verifyPhoneNumber(options);
+    }
     public void KisanLoginButtonOnClickListener(View view) {
+        verifySignIncode();
+    }
+    private void verifySignIncode(){
+        String code = kisanOTP.getText().toString();
+        if(code.isEmpty()){
+            Toast.makeText(getApplicationContext(),"Enter OTP",Toast.LENGTH_LONG).show();
+            return;
+        }
+        PhoneAuthCredential credential = PhoneAuthProvider.getCredential(codeSent, code);
+        signInWithPhoneAuthCredential_LOGIN(credential);
+    }
+    private void signInWithPhoneAuthCredential_LOGIN(PhoneAuthCredential credential) {
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            Toast.makeText(getApplicationContext(),"Login Successful",Toast.LENGTH_LONG).show();
+                            FirebaseUser user = task.getResult().getUser();
+                            long creationTimestamp = user.getMetadata().getCreationTimestamp();
+                            long lastSignInTimestamp = user.getMetadata().getLastSignInTimestamp();
+                            if (creationTimestamp == lastSignInTimestamp) {
+                                Toast.makeText(getApplicationContext(),"Please create Account first",Toast.LENGTH_LONG).show();
+                            } else {
+                                Intent intent = new Intent(getApplicationContext(),Famers_Dashboard.class);
+                                startActivity(intent);
+                            }
+                        } else {
+                            // Sign in failed, display a message and update the UI
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            if (task.getException() instanceof FirebaseAuthInvalidCredentialsException) {
+                                Toast.makeText(getApplicationContext(),"Incorrect Verification code",Toast.LENGTH_LONG).show();
+
+                            }
+                        }
+                    }
+                });
     }
 
     public void KisanCreateAccountButtonOnClickListener(View view) {
+        consumerCardView.setVisibility(View.GONE);
+        kisanCardView.setVisibility(View.GONE);
+        KisanCreateAccountCardView.setVisibility(View.VISIBLE);
+    }
+    public void farmer_Create_AC_OTP_Generate(View view) {
+    }
+    public void Create_Farmer_AC(View view) {
     }
 
-    public void KisanForgortPasswordOnClickListener(View view) {
-    }
-
-
+// Consumer CardView Functions
     public void ConsumerLoginOnClickListener(View view) {
     }
 
@@ -81,4 +216,39 @@ public class MainActivity extends AppCompatActivity {
 
     public void consumerForgotPasswordOnClickListener(View view) {
     }
+    /*@Override
+    public void onBackPressed()
+    {
+        kisanCardView.setVisibility(View.GONE);
+        consumerCardView.setVisibility(View.GONE);
+    }*/
+    // Backbutton functions
+    int doubleBackToExitPressed = 1;
+    @RequiresApi
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressed == 1) {
+            kisanCardView.setVisibility(View.GONE);
+            consumerCardView.setVisibility(View.GONE);
+            KisanCreateAccountCardView.setVisibility(View.GONE);
+        }
+        if (doubleBackToExitPressed == 2) {
+            finishAffinity();
+            System.exit(0);
+        }
+        else {
+            doubleBackToExitPressed++;
+            Toast.makeText(this, "Please press Back again to exit", Toast.LENGTH_SHORT).show();
+        }
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                doubleBackToExitPressed=1;
+            }
+        }, 2000);
+    }
+
+
+
 }
